6b6e67d11c04bf07633974e6f9d04421bf336cec
Akondi Sai Karthik Vyas <vyasakondi009@gmail.com>
2023-11-14 22:14:27 -0500

    Binary Search folder added

---
+++ Arrays/Binary Search/square_root.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        long long int start=0;
+        long long int end=x;
+        long long int temp=0;
+        
+        while(start<=end)
+        {
+            long long int mid=(start+end)/2;
+            if(mid*mid==x)
+            {
+                return mid;
+            }
+            else
+            {
+                if(mid*mid>x)
+                {
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                                    temp=mid;
+
+                }
+            }
+        }
+        
+        return  temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
